0b39c10ac24bbf499b19c7dc12dd68b29709b23d
giuliocarot0 <s244930@studenti.polito.it>
2022-01-09 12:37:02 +0100

    add decoding to sample app

--- main.cpp
+++ main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 #include "SCPPVideoInput.h"
 #include "SCPPAudioInput.h"
+#include "SCPPDecoder.h"
 
 
 int main() {
+    AVPacket *inPacket;
+    AVFrame *rawFrame;
+
+    rawFrame = av_frame_alloc();
+    inPacket = av_packet_alloc();
+    avdevice_register_all();
+
     //Open two input devices
-    SCPPVideoInput scppVideoInput("x11grab", "1:none", SRResolution{1920,1080}, SROffset{0,0}, 30 );
-    scppVideoInput.open();
-    AVFormatContext* test_video = scppVideoInput.getFormatContext();
+    SCPPVideoInput videoInput("avfoundation", "1:none", SRResolution{2560,1600}, SROffset{0,0}, 30 );
+    SCPPDecoder videoDecoder;
+
+
+    videoInput.open();
+    videoDecoder.setDecoderContext(videoInput.getCodecContext());
+
+    int flag = 0;
+    while(!flag) {
+
+
+        if(videoInput.readPacket(inPacket) >= 0){
+            printf("PacketPTS: %lld\n",inPacket->pts);
+            videoDecoder.decodePacket(inPacket);
+            while(videoDecoder.getDecodedFrame(rawFrame)>=0){
+                printf("\t decodedFrame %lld\n", rawFrame->pts);
+            }
+            av_packet_unref(inPacket);
+
+        }
+        else flag = 1;
+    }
 
     return 0;
 }
