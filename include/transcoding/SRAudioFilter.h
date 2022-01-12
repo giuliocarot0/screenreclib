d850958ebe603869b73072c82a6b9c410ef47934
AleLuc98 <47351482+AleLuc98@users.noreply.github.com>
2022-01-12 18:46:56 +0100

    Refactor

--- include/transcoding/SRAudioFilter.h
+++ include/transcoding/SRAudioFilter.h
@@ -20,9 +20,6 @@ private:
 public:
     SRAudioFilter ( AVCodecContext *encoder, AVCodecContext *decoder): encoder(encoder), decoder(decoder), resampling_context(nullptr){};
     void init();
-    int init_fifo(AVCodecContext * outACodecContext);
-    int add_samples_to_fifo(uint8_t **converted_input_samples, const int frame_size);
-    int initConvertedSamples(uint8_t ***converted_input_samples, AVCodecContext *output_codec_context, int frame_size);
     AVFrame* filterFrame(AVFrame* inputFrame);
 
     AVAudioFifo *getFifo() const;
